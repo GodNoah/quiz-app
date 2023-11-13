02f23d1379eb9ea196692037ad723739f5f1eae8
GodNoah <121417271+GodNoah@users.noreply.github.com>
2023-11-13 09:03:25 +0630

    Update App.jsx

--- quiz-react-app/src/App.jsx
+++ quiz-react-app/src/App.jsx
@@ -1,9 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+
+
+  return (
+  <div>
+    {/* Heading */}
+    <h1>Quiz App</h1>
+    <h3>QUestion 1 of 5</h3>
+    <p>Current Score</p>
+
+  
+    {/* Question answer */}
+    <div className='box1'>
+       <h3>Q : </h3>
+    
+    
+    </div>
+
+
+    {/* REsult */}
+    <div className='box2'></div>
+  
+  </div> 
+  )
+
 }
 
 export default App
