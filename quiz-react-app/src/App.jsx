52d3257932227a97e7e711d2880054817185c998
GodNoah <121417271+GodNoah@users.noreply.github.com>
2023-11-13 20:37:45 +0630

    Answer Updated

--- quiz-react-app/src/App.jsx
+++ quiz-react-app/src/App.jsx
@@ -10,52 +10,52 @@ function App() {
     {
       question: "Which planet is closet to the Sun?",
       choices : [
-        {id : 1, text: ".....", isCorrect: false},
-        {id : 2, text: ".....", isCorrect: false},
-        {id : 3, text: ".....", isCorrect: false},
-        {id : 4, text: ".....", isCorrect: false}
+        {id : 1, text: "Earth", isCorrect: false},
+        {id : 2, text: "Uranes", isCorrect: false},
+        {id : 3, text: "Venus", isCorrect: false},
+        {id : 4, text: "Mercury", isCorrect: false}
       ]
     },
     {
       question: "Which drink is ameican's signature?",
       choices : [
-        {id : 1, text: ".....", isCorrect: false},
-        {id : 2, text: ".....", isCorrect: false},
-        {id : 3, text: ".....", isCorrect: false},
-        {id : 4, text: ".....", isCorrect: false}
+        {id : 1, text: "StarBuck", isCorrect: false},
+        {id : 2, text: "Coca-cola", isCorrect: false},
+        {id : 3, text: "Whiskey", isCorrect: false},
+        {id : 4, text: "Beer", isCorrect: false}
       ]
     },
     {
       question: "Which ocean has largest area in the world?",
       choices : [
-        {id : 1, text: ".....", isCorrect: false},
-        {id : 2, text: ".....", isCorrect: false},
-        {id : 3, text: ".....", isCorrect: false},
-        {id : 4, text: ".....", isCorrect: false}
+        {id : 1, text: "Atlantic", isCorrect: false},
+        {id : 2, text: "Pacific", isCorrect: false},
+        {id : 3, text: "Arctic", isCorrect: false},
+        {id : 4, text: "Indian", isCorrect: false}
       ]
     },
     {
       question: "How many Sub-particle are there in atom?",
       choices : [
-        {id : 1, text: ".....", isCorrect: false},
-        {id : 2, text: ".....", isCorrect: false},
-        {id : 3, text: ".....", isCorrect: false},
-        {id : 4, text: ".....", isCorrect: false}
+        {id : 1, text: "one", isCorrect: false},
+        {id : 2, text: "two", isCorrect: false},
+        {id : 3, text: "three", isCorrect: false},
+        {id : 4, text: "four", isCorrect: false}
       ]
     },
     {
       question: "Who wrote pride and prejudice",
       choices : [
-        {id : 1, text: ".....", isCorrect: false},
-        {id : 2, text: ".....", isCorrect: false},
-        {id : 3, text: ".....", isCorrect: false},
-        {id : 4, text: ".....", isCorrect: false}
+        {id : 1, text: "Charles Dickens", isCorrect: false},
+        {id : 2, text: "Jane Austin", isCorrect: false},
+        {id : 3, text: "Shakespeare", isCorrect: false},
+        {id : 4, text: "Hemingway", isCorrect: false}
       ]
     },
     {
       question: "Most handsome human being",
       choices : [
-        {id : 1, text: ".....", isCorrect: false},
+        {id : 1, text: "", isCorrect: false},
         {id : 2, text: ".....", isCorrect: false},
         {id : 3, text: ".....", isCorrect: false},
         {id : 4, text: ".....", isCorrect: false}
